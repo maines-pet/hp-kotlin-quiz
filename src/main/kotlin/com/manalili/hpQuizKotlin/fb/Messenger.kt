e790a744bd8af4c7bd3ddaa3df021f39f85c411f
kevin <ksmanalili@gmail.com>
2019-03-21 16:56:17 +1100

    Refactor sending of simple messages to allow multiple messages in one function call

--- src/main/kotlin/com/manalili/hpQuizKotlin/fb/Messenger.kt
+++ src/main/kotlin/com/manalili/hpQuizKotlin/fb/Messenger.kt
@@ -3,9 +3,6 @@ package com.manalili.hpQuizKotlin.fb
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.manalili.hpQuizKotlin.fb.received.PostbackMessage
 import com.manalili.hpQuizKotlin.fb.received.SimpleMessage
-import com.manalili.hpQuizKotlin.fb.send.MessageContent
-import com.manalili.hpQuizKotlin.fb.send.MessageToSend
-import com.manalili.hpQuizKotlin.fb.send.MessagingType
 import com.manalili.hpQuizKotlin.service.GameService
 import org.springframework.stereotype.Service
 
@@ -20,9 +17,11 @@ class Messenger(val sendApi: SendService,
         val sender = gameService.players[simpleMessage.sender.id]!!
         if (sender.needName){
             sender.updateName(simpleMessage.message.text)
-            val reply = """Thanks, ${sender.name}"""
-            val result = this.sendApi.send(endPoint = SendService.MESSAGES,
-                    body = MessageToSend(MessagingType.RESPONSE, sender.id, MessageContent(reply)))
+            val reply =
+                    listOf("""Thanks, ${sender.name}.""",
+                            "Please wait for other players.",
+                            "I'll let you know once we're ready.")
+            this.sendApi.sendSimpleReply(sender.id, replyList = reply)
         }
     }
 
@@ -32,23 +31,12 @@ class Messenger(val sendApi: SendService,
             "get_started" -> getStarted(postbackMessage)
         }
     }
-
     private fun getStarted(postbackMessage: PostbackMessage) {
-        gameService.addPlayer(postbackMessage.sender.id)
-        listOf("Before we start",
-                "how would you like to be called?")
-                .forEach {
-                    sendApi.send(
-                            endPoint = SendService.MESSAGES,
-                            body = MessageToSend(MessagingType.RESPONSE,
-                                    postbackMessage.sender.id,
-                                    MessageContent(it))
-                    )
-                }
-//        val playerProfile = profileApi.retrieveProfile(postbackMessage.sender.id)
-//        if (playerProfile != null) {
-//            playerService.playerList.add(Player(playerProfile.id, playerProfile.firstName))
-//        }
+
+        val sender = postbackMessage.sender.id
+        gameService.addPlayer(sender)
+        sendApi.sendSimpleReply(sender, replyList = listOf("Before we start",
+                "how would you like to be called?"))
     }
 
 
--- src/main/kotlin/com/manalili/hpQuizKotlin/fb/SendService.kt
+++ src/main/kotlin/com/manalili/hpQuizKotlin/fb/SendService.kt
@@ -1,13 +1,19 @@
 package com.manalili.hpQuizKotlin.fb
 
+import com.manalili.hpQuizKotlin.fb.send.MessageContent
+import com.manalili.hpQuizKotlin.fb.send.MessageToSend
+import com.manalili.hpQuizKotlin.fb.send.MessagingType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
+import sun.plugin2.message.Message
+import java.awt.TrayIcon
 
 @Service
-class SendService(val fb: FbProperties){
+class SendService(val fb: FbProperties) {
 
+    //General API for sending POST Request to fb messenger
     fun send(endPoint: String, psId: String = "/me", body: Any): ResponseEntity<String> {
         val uri = UriComponentsBuilder.newInstance()
                 .scheme("https")
@@ -19,6 +25,17 @@ class SendService(val fb: FbProperties){
                 .build().toUri()
         return RestTemplate().postForEntity(uri, body, String::class.java)
     }
+
+    fun sendSimpleReply(id: String, singleReply: String = "", replyList: List<String> = listOf()) {
+        replyList.plus(singleReply).forEach {
+            if(it.isNotEmpty()){
+                this.send(SendService.MESSAGES,
+                        body = MessageToSend(MessagingType.RESPONSE, recipient = id,
+                                message = MessageContent(it)))
+            }
+        }
+    }
+
 
     companion object {
         val MESSAGES = "/messages"
